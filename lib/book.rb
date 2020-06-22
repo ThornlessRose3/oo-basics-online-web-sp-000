52bea0bedf309e7483942f05a4d1fd020bcb0688
Carrie Jones <c.jessica.jones@gmail.com>
2020-06-22 21:51:33 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -8,5 +8,22 @@ class Book
     @title
   end
   
+  def author=(author)
+    @author = author
+  end
+  
+  def author
+    @author
+  end
+  
+  def page_count=(num)
+    @page_count = num
+  end
+  
+  def page_count
+    @page_count
+  end
+  
+  
 end
 
